5aaf624fee8e5755d0cf8cebd45c18df71a4e74a
Wassim <96482486+On1zuma@users.noreply.github.com>
2022-12-07 08:36:15 +0000

    feat : aixmazon logo

--- storefront/features/Header/Header.tsx
+++ storefront/features/Header/Header.tsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 import { Heading } from "../../components";
 import Navbar from "../../containers/Navbar/Navbar";
-import Image from "next/image";
-
 
 const Header = () => {
   return (
     <Wrapper>
-        <Image src="/amazon.png" alt="Aixmazone" width={189} height={60} />
+      <Image src="/aixmazon.png" alt="Aixmazone" width={189} height={60} />
       <Navbar />
     </Wrapper>
   );
